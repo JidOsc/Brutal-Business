9c331fe4b6549d0d69257399badbfb3ad40b66fb
CrushingCheese <love.jonsson@elev.ga.lbs.se>
2024-02-05 11:33:31 +0100

    SeesPlayer

--- Spelet/Enemy.cs
+++ Spelet/Enemy.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Spelet
 {
     internal class Enemy : HPEntity
     {
+        float viewDistance = 1.1f;
+        
+        public bool SeesPlayer(Player player)
+        {
+            float distanceToPlayer = (float)Math.Sqrt(Math.Pow(player.position.X - position.X, 2) + Math.Pow(player.position.Y - position.Y, 2));
+
+            if (distanceToPlayer <= viewDistance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void ChasePlayer(Player player)
+        {
+            velocity = Vector2.Normalize(position - player.position) * speed * -1;         
+        }
+
+        public void Update()
+        {
+
+        }
+
+
     }
 }
