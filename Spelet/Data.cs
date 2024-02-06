2c96773863ffff0a02b650bd1574bf5c0a0d07f3
CrushingCheese <love.jonsson@elev.ga.lbs.se>
2024-02-06 09:50:17 +0100

    Keyboard

--- Spelet/Data.cs
+++ Spelet/Data.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +11,21 @@ namespace Spelet
 {
     internal class Data
     {
+        public static KeyboardState keyboard, lastKeyboard;
+
         public static Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>()
         {
-            
+            {"player", null },
+            {"enemy", null },
+            {"tilemap", null }
         };
 
-        
+        public static void LoadContent(ContentManager _contentManager)
+        {
+            
+            
+
+            //loopa igenom dictionary för att med hjälp av contentmanager ladda in alla texturer
+        }
     }
 }
