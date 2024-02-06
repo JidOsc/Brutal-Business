f71f59a7104639cc4fce6846c7d2577329c54f65
JidOsc <84289227+JidOsc@users.noreply.github.com>
2024-02-06 09:47:13 +0100

    bunch of functions

--- Spelet/Hitbox.cs
+++ Spelet/Hitbox.cs
@@ -14,5 +14,14 @@ namespace Spelet
         public Vector2
             position,
             size;
+
+        public bool IsInside(Vector2 otherPosition)
+        {
+            return 
+                otherPosition.X >= position.X 
+                && otherPosition.X <= position.X + size.X
+                && otherPosition.Y >= position.Y
+                && otherPosition.Y <= position.Y + size.Y;
+        }
     }
 }
