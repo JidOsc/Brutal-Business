677ee32f0ffa6cf14f9a33343a489be99b16a3db
CrushingCheese <love.jonsson@elev.ga.lbs.se>
2024-02-06 09:36:01 +0100

    Create Data.cs

---
+++ Spelet/Data.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spelet
+{
+    internal class Data
+    {
+        public static Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>()
+        {
+            
+        };
+
+        
+    }
+}
