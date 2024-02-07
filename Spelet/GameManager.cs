179b0755b1ed596fbfa9dd4a4bbe8db7764387e3
JidOsc <84289227+JidOsc@users.noreply.github.com>
2024-02-07 14:22:09 +0100

    gamestates!

--- Spelet/GameManager.cs
+++ Spelet/GameManager.cs
@@ -6,27 +6,68 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
 
 namespace Spelet
 {
     internal class GameManager
     {
         public MapManager mapManager;
+        public MenuManager menuManager;
+
+        public enum GameState {main, ingame};
+        public GameState currentGameState = new();
+
+        string filepathFolder;
+
 
         public GameManager()
         {
             mapManager = new MapManager();
+            menuManager = new MenuManager();
+
+            currentGameState = GameState.main;
+
             //short[][] map = LoadMap();
         }
 
         public void Update(GameTime _gameTime)
         {
-            mapManager.Update(_gameTime);
+            switch (currentGameState)
+            {
+                case GameState.main:
+                    menuManager.Update(_gameTime);
+                    break;
+
+                case GameState.ingame:
+                    mapManager.Update(_gameTime);
+                    break;
+            }
         }
 
         public void Draw(SpriteBatch _spriteBatch)
         {
-            mapManager.Draw(_spriteBatch);
+            switch (currentGameState)
+            {
+                case GameState.main:
+                    menuManager.Draw(_spriteBatch);
+                    break;
+
+                case GameState.ingame:
+                    mapManager.Draw(_spriteBatch);
+                    break;
+            }
+        }
+
+
+        public void AccessFolder()
+        {
+            filepathFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"BrutalBusiness\");
+
+            if (!Directory.Exists(filepathFolder))
+            {
+                Directory.CreateDirectory(filepathFolder);
+            }
         }
 
         public void SaveMap()
--- Spelet/MenuManager.cs
+++ Spelet/MenuManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Spelet
 {
@@ -12,6 +15,15 @@ namespace Spelet
         public MenuManager()
         {
             Mainmenu = new Menu();
+        }
+
+        public void Update(GameTime _gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch _spriteBatch)
+        {
 
         }
     }
