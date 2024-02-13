d45adf7d3164529a091eba9a1261dbdb45ccbe6b
CrushingCheese <love.jonsson@elev.ga.lbs.se>
2024-02-13 13:53:35 +0100

    push

--- Spelet/Progressbar.cs
+++ Spelet/Progressbar.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,67 @@ using System.Threading.Tasks;
 
 namespace Spelet
 {
-    internal class Progressbar
+    internal class Progressbar:UIElement
     {
+        Texture2D foreground;
+
+        Rectangle foregroundHitbox;
+
+
+
+        float
+            maxValue,
+            currentValue,
+            oldValue,
+            slideSpeed = 0.2f,
+            newValue;
+
+        
+
+        public Progressbar(Texture2D foreground, Vector2 position, Vector2 size, float max, Texture2D background) : base(position,size)
+        {
+            this.foreground = foreground;
+            this.background = background;
+            maxValue = max;
+            currentValue = max;
+            this.position = position;
+            foregroundHitbox = new(0, 0, foreground.Width, foreground.Height);
+
+        }
+
+        void Update(GameTime gameTime)
+        {
+            if (Data.keyboard.IsKeyDown(Keys.LeftShift))
+            {
+                SlideValue(currentValue - 0.2f);
+            }
+            else if(Data.keyboard.IsKeyDown(Keys.LeftShift)== false)
+            {
+                SlideValue(currentValue + 0.2f);
+            }
+            UpdateSlider();
+        }
+
+        void UpdateSlider()
+        {
+            if(currentValue != newValue)
+            {
+                currentValue = oldValue + (newValue - oldValue) * slideSpeed;
+                foregroundHitbox.Width = (int)(currentValue * foreground.Width);
+            }
+        }
+
+        public void SlideValue(float Value)
+        {
+            newValue = Value;
+            oldValue = currentValue;
+            
+        }
+
+        void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(foreground,position, foregroundHitbox,Color.AliceBlue);
+        }
+
     }
 }
