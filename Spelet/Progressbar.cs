be93e4c4dae3f92de38801d6eb4944b51e4850c6
CrushingCheese <love.jonsson@elev.ga.lbs.se>
2024-02-14 10:55:01 +0100

    stamina bar

--- Spelet/Progressbar.cs
+++ Spelet/Progressbar.cs
@@ -18,34 +18,38 @@ namespace Spelet
 
 
         float
-            maxValue,
+            maxValue = 5f,
             currentValue,
             oldValue,
-            slideSpeed = 0.2f,
-            newValue;
+            slideSpeed = 0.1f,
+            newValue,
+            slide;
 
         
 
-        public Progressbar(Texture2D foreground, Vector2 position, Vector2 size, float max, Texture2D background) : base(position,size)
+        public Progressbar(Texture2D foreground, Vector2 position, Vector2 size, float currentvalue, Texture2D background) : base(position,size)
         {
             this.foreground = foreground;
             this.background = background;
-            maxValue = max;
-            currentValue = max;
+
+            this.currentValue = currentvalue;
             this.position = position;
             foregroundHitbox = new(0, 0, foreground.Width, foreground.Height);
 
         }
 
-        void Update(GameTime gameTime)
+        public void Update(GameTime gameTime)
         {
-            if (Data.keyboard.IsKeyDown(Keys.LeftShift))
+            if (Data.keyboard.IsKeyDown(Keys.LeftShift) && currentValue>0)
             {
                 SlideValue(currentValue - 0.2f);
             }
-            else if(Data.keyboard.IsKeyDown(Keys.LeftShift)== false)
+            else
             {
-                SlideValue(currentValue + 0.2f);
+                if (currentValue < maxValue)
+                {
+                    SlideValue(currentValue + 0.2f);
+                }
             }
             UpdateSlider();
         }
@@ -54,13 +58,22 @@ namespace Spelet
         {
             if(currentValue != newValue)
             {
-                currentValue = oldValue + (newValue - oldValue) * slideSpeed;
+                slide += slideSpeed;
+
+                currentValue = oldValue + (newValue - oldValue) * slide;
                 foregroundHitbox.Width = (int)(currentValue * foreground.Width);
             }
+            else
+            {
+                slide = 0;
+            }
+            
         }
 
         public void SlideValue(float Value)
         {
+            slide = 0;
+
             newValue = Value;
             oldValue = currentValue;
             
@@ -68,9 +81,9 @@ namespace Spelet
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            spriteBatch.Draw(Data.textures["background"],foregroundHitbox, Color.Green);
+            spriteBatch.Draw(Data.textures["background"], foregroundHitbox, Color.Red);
             spriteBatch.Draw(foreground, position, foregroundHitbox,Color.White);
-            spriteBatch.Draw(Data.textures["background"],foregroundHitbox,Color.Green);
         }
-
     }
 }
