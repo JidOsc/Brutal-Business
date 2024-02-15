fd76502d011cfb12d8bf2dceb40814659f9eddc0
Le0nLe0n <158146597+Le0nLe0n@users.noreply.github.com>
2024-02-15 12:34:29 +0100

    Update SoundManager.cs

--- Spelet/SoundManager.cs
+++ Spelet/SoundManager.cs
@@ -4,13 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
 
 namespace Spelet
 {
     
     internal class SoundManager
     {
-       // public static void PlaySound(Vector2, position)
+        public static void PlaySound(Vector2 position, SoundEffect sound )
+        {
+            sound.Play();
+        }
 
     }
 } 
