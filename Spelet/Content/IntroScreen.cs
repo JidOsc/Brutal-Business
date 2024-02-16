8f5e560315c79d8f0a4847dca91fccf8f02cb4ce
CrushingCheese <love.jonsson@elev.ga.lbs.se>
2024-02-16 11:20:19 +0100

    forgot to save

--- Spelet/Content/IntroScreen.cs
+++ Spelet/Content/IntroScreen.cs
@@ -8,5 +8,19 @@ namespace Spelet.Content
 {
     internal class IntroScreen
     {
+        Animation intro;
+
+        public IntroScreen()
+        {
+            intro = new Animation(0, 6, 0.3f, 0);
+        }
+
+         
+
+
+
+
+
+
     }
 }
