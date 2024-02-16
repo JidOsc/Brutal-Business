a30d61a8e15a0bf32e8071e92de967097b413fda
JidOsc <84289227+JidOsc@users.noreply.github.com>
2024-02-16 14:48:14 +0100

    inventory margin

--- Spelet/UIManager.cs
+++ Spelet/UIManager.cs
@@ -19,6 +19,8 @@ namespace Spelet
             playerHealth,
             playerStamina;
 
+        const int inventoryMargin = 50;
+
         public UIManager()
         {
             inventorybar = new InventoryView[3];
